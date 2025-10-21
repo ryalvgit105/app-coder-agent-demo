452e7a256d416e92aade7df078007fbb43754df6
ryalvgit105 <ryan.alvarado.gsp@gmail.com>
2025-10-20 21:44:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
+
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello from Railway!"}
+    return {"message": "Hello from Replit!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
